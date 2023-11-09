b1fa028d4a7dee193438f1bc3358d6cf9607552e
pipichaan <147584671+pipichaan@users.noreply.github.com>
2023-11-09 13:22:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите значения массива с обязательными двумя нулями");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] mas = new int[a];
+int nol1 = 0;
+int nol2 = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i]= Convert.ToInt32(Console.ReadLine());
+
+}
+for( int i = 0;i < mas.Length; i++) 
+{ 
+    if (mas[i] == 0)
+    {
+        nol1 = i;
+        break;
+    }
+}
+for (int i = nol1 + 1; i < mas.Length; i++)
+{
+    if (mas[i] == 0)
+    {
+        nol2 = i;
+        break;
+    }
+}
+for (int i = nol1 + 1; i <= nol2 - 1; i++)
+{
+    Console.WriteLine(mas[i]);
+}
